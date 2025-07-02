c88baedb6e7a9687cd5f3632b63a886ef6d2e0de
Anuradha Singh <singhanuradha0066@gmail.com>
2025-07-02 11:02:56 +0530

    String

---
+++ Concatenate.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class Concatenate {
+    public static void main(String[] args) {
+       String firstName="Anu";
+       String lastName="Radha"; 
+       String name=firstName+lastName;
+       System.out.println(name);
+    }
+}
