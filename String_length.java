c31560dbbeee924abf9ec72c6e27723580c70fda
Anuradha Singh <singhanuradha0066@gmail.com>
2025-07-02 11:03:50 +0530

    String

---
+++ String_length.java
@@ -0,0 +1,8 @@
+package Strings;
+
+public class String_length {
+    public static void main(String[] args) {
+     String str="hello World";
+     System.out.println("length: "+str.length());
+    }
+}
