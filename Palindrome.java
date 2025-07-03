ff537dc8c4e6bcff15a4aa97a87e076b26de00f5
Anuradha Singh <singhanuradha0066@gmail.com>
2025-07-03 21:55:43 +0530

    Array

--- Palindrome.java
+++ Palindrome.java
@@ -1,20 +1,32 @@
-package Strings;
+package Arr;
+
+import java.util.Arrays;
 
 public class Palindrome {
     public static void main(String[] args) {
-        String str="past";
-       str=str.toLowerCase();
-        String reverse="";
-        for(int i=str.length()-1;i>=0;i--){
-            reverse+=str.charAt(i);
+        int[] arr={1,2,3,2,1};
+        int[] rev=new int[arr.length]; 
+       for(int i=arr.length-1,j=0;i>=0;i--,j++){
+        rev[j]=arr[i];
+       }
+       String revs=Arrays.toString(rev);
+     String arrs=Arrays.toString(arr);
 
-        }
-        if(str.equals(reverse)){
-            System.out.println("palindrome");
-        }
-        else{
-            System.out.println("not pallindrome");
-        }
-       
+    //    if(revs.equals(arrs)){
+    //     System.out.println("palindrome");
+    //    }
+    //    else{
+    //     System.out.println("not palindrome");
+    //    }
+  
+   boolean a= isBool(arrs, revs);
+   System.out.println(a);
+    }
+    
+    public static boolean isBool(String arrs,String revs){
+   if(revs.equals(arrs)){
+    return true;
+   }
+   return false;
     }
 }
