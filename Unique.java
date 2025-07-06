93a40ca8c3e0afaaaf192985c17ee1bd144e708f
Anuradha Singh <singhanuradha0066@gmail.com>
2025-07-06 19:43:35 +0530

    Array

--- Unique.java
+++ Unique.java
@@ -1,22 +1,34 @@
-package Array_1D;
+package DSA;
 
 public class Unique {
     public static void main(String[] args) {
-         int[] arr={1,2,3,4};
+        int[] arr={1,2,3,4,5};
         int[] arr1={3,4,5,6};
-        for(int i=0;i<arr.length;i++){
-        boolean data=true;
-            for(int j=0;j<arr1.length;j++){
-             if(arr[i]==arr1[j]){
-                data=false;
-                //break;
-             }
-            }
-            if(data==true){
-                System.out.println(arr[i]);
-            }
 
-            
+        for(int i=0;i<arr.length;i++){
+            boolean data=true;
+            for(int j=0;j<arr1.length;j++){
+                if(arr[i]==arr1[j]){
+                    data=false;
+                    break;
+                }
+            }
+        if(data==true){
+        System.out.println(arr[i]+" ");
+        }}
+
+        for(int k=0;k<arr1.length;k++){
+            boolean data1=true;
+            for(int j1=0;j1<arr.length;j1++){
+                if(arr[j1]==arr1[k]){
+                    data1=false;
+                    break;
+                }
+            }
+        if(data1==true){
+        System.out.println(arr1[k]+" ");
         }
+    
+    }
     }
 }
