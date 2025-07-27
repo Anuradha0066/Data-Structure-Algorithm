6434f40cbedaa0dac3dde13b8a5fae5e639acc59
Anuradha Singh <singhanuradha0066@gmail.com>
2025-07-27 15:20:40 +0530

    Add files via upload

---
+++ SubarraySum.java
@@ -0,0 +1,10 @@
+package Practice_Question;
+
+public class SubarraySum {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 20, 3, 10, 5};
+         int target = 33;
+         
+         
+    }
+}
